3e3fd2f79aa17e52cd1b2e4dc1bb82d5460a158a
ujangbedog <domba88galing@gmail.com>
2022-09-01 23:03:12 +0700

    add a class component

---
+++ src/components/Message.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+class Message extends Component
+{
+    render()
+    {
+        return <h1>Hello World from a Class Component!</h1>;
+    }
+}
+
+export default Message;
