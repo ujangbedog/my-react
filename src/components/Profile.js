66178b341d066f4c90575a3760437e2660650735
ujangbedog <domba88galing@gmail.com>
2022-09-01 23:44:57 +0700

    add props component functional

---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+function Profile(props)
+{
+    console.log(props);
+    //return <h1>Name: { props.name }</h1>
+    return( 
+        <h1>
+            Name: { props.name } { props.lastname }
+            { props.children }
+        </h1>
+    );
+}
+
+export default Profile;
