a612a90f476b7da3b7682410a5659b648be850e1
ujangbedog <domba88galing@gmail.com>
2022-09-01 23:45:40 +0700

    configuration props component functional

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 //import Functional Component from 'Path file'
-import Hello from './components/Hello';
-import Message from './components/Message';
+//import Hello from './components/Hello';
+//import Message from './components/Message';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
 
-      {/* import functional component Hello */}
+      {/* import functional component Hello
       <Hello />
 
-      {/* import a class component Message */}
-      <Message />
-      
+      import a class component Message
+      <Message /> */}
+
+      {/* import props functional component Profile */}
+      <Profile name="Xoejang" />
+      <Profile name="Xoejang" lastname="Bedog"/>
+
+      {/* import props functional component with Children Hello */}
+      <Profile name="Xoejang" lastname="Bedog">
+        <h3>This is Children of Profile Component.</h3>
+      </Profile>
     </div>
   );
 }
