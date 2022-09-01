fbd528c49c2d7de439c2a08db2d2db363db054be
ujangbedog <domba88galing@gmail.com>
2022-09-01 23:12:51 +0700

    JSX method in functional components

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,6 +1,15 @@
+//const name = "Xoejang";
+
+const displayMessage = () => 
+{
+    return "I Love You";
+}
+
 function Hello()
 {
-    return <h1>Hello World!</h1>
+    //return <h1>Hello World! { name } </h1>
+    //return <h1>10 + 20 = { 10 + 20 } </h1>
+    return <h1>The Message is: { displayMessage() } </h1>
 }
 
 //another syntax
