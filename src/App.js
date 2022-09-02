aaf276870a2c8e2437650cb552bb9b7af6c182cf
ujangbedog <domba88galing@gmail.com>
2022-09-02 18:53:14 +0700

    configure App.js for props from a class

--- src/App.js
+++ src/App.js
@@ -2,27 +2,28 @@
 import './App.css';
 //import Functional Component from 'Path file'
 //import Hello from './components/Hello';
-//import Message from './components/Message';
-import Profile from './components/Profile';
+import Message from './components/Message';
+//import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
 
-      {/* import functional component Hello
-      <Hello />
+      {/* import functional component Hello */}
+      {/* <Hello /> */}
 
-      import a class component Message
-      <Message /> */}
+      {/* import a class component Message */}
+      <Message messagecontent="This is a message from props"/>
+      <Message messagecode="28" messagecontent="This is a message from props"/>
 
       {/* import props functional component Profile */}
-      <Profile name="Xoejang" />
-      <Profile name="Xoejang" lastname="Bedog"/>
+      {/* <Profile name="Xoejang" />
+      <Profile name="Xoejang" lastname="Bedog"/> */}
 
       {/* import props functional component with Children Hello */}
-      <Profile name="Xoejang" lastname="Bedog">
+      {/* <Profile name="Xoejang" lastname="Bedog">
         <h3>This is Children of Profile Component.</h3>
-      </Profile>
+      </Profile> */}
     </div>
   );
 }
